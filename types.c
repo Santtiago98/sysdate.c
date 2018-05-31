92466a45b70ac3ba94e7ef9ec906de0cfb243567
Santtiago98 <39803285+Santtiago98@users.noreply.github.com>
2018-05-31 12:39:06 -0300

    Create types.c

---
+++ types.c
@@ -0,0 +1,15 @@
+#ifndef TYPES__H
+#define TYPES__H
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "errors.h"
+
+#define MAX_FORMATS 5
+#define NUMBER_OF_ARGUMENTS 3
+#define FORMAT_POSC 2
+#define MAX_SPACE 50
+#define FORMAT_FLAG "-fmt"
+
+#endif
